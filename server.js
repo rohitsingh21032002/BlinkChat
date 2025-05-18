8ce248fbf1f110fb74cbbfe96c5b01db89442a6d
rohitsingh20020321 <rsrs6958@gmail.com>
2025-05-18 17:55:03 +0530

    BlinkChart initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public")); // assuming index.html is in /public
+
+const onlineUsers = {};
+
+io.on("connection", (socket) => {
+  console.log("New user connected:", socket.id);
+
+  socket.on("user_joined", (name) => {
+    console.log(name);
+    onlineUsers[socket.id] = name;
+    console.log("User joined:", name);
+
+    io.emit("user_joined_announcement", name);
+    io.emit("update_user_list", Object.values(onlineUsers));
+  });
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("typing", () => {
+    const name = onlineUsers[socket.id];
+    if (name) {
+      socket.broadcast.emit("user_typing", name);
+    }
+  });
+
+  socket.on("stop_typing", () => {
+    socket.broadcast.emit("user_stopped_typing");
+  });
+
+  socket.on("disconnect", () => {
+    const name = onlineUsers[socket.id];
+    console.log("User disconnected:", name);
+
+    delete onlineUsers[socket.id];
+    io.emit("update_user_list", Object.values(onlineUsers));
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server listening on http://localhost:3000");
+});
